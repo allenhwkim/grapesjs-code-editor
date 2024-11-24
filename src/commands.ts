bd1b7dcc8001ededff696570dfc3383a50768fd9
Kim, Allen <allen.kim@ontariohealth.ca>
2024-11-24 14:09:31 -0500

    removed unused command code-editor-object and code-editor-constructor

--- src/commands.ts
+++ src/commands.ts
@@ -2,10 +2,9 @@ import { Editor } from 'grapesjs';
 import { CodeEditor } from './code-editor';
 
 export default (editor: Editor, opts) => {
-  const cm = editor.Commands;
   let codeEditor: CodeEditor | null = null;
 
-  cm.add('open-code', {
+  editor.Commands.add('open-code', {
     run: editor => {
       !codeEditor && (codeEditor = new CodeEditor(editor, opts)) && codeEditor.buildCodePanel();
       codeEditor.showCodePanel();
@@ -13,13 +12,5 @@ export default (editor: Editor, opts) => {
     stop: editor => {
       codeEditor && codeEditor.hideCodePanel();
     },
-  });
-
-  cm.add('code-editor-object', () => {
-    return codeEditor;
-  });
-
-  cm.add('code-editor-constructor' , () => {
-    return CodeEditor;
   });
 }
