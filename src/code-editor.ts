fd37a4133cb945e0bbde04840198bf1d1c549c21
Kim, Allen <allen.kim@ontariohealth.ca>
2024-11-24 19:36:27 -0500

    simplified

--- src/code-editor.ts
+++ src/code-editor.ts
@@ -1,100 +1,81 @@
-//Original work Copyright (c) 2018, Duarte Henriques, https://github.com/portablemind/grapesjs-code-editor
-//Modified work Copyright (c) 2020, Brendon Ngirazi,
-//All rights reserved.
-
-import { Component, Editor } from 'grapesjs';
-import Split from 'split.js';
+import { Component, Editor, Panel } from 'grapesjs';
+import split from 'split.js';
 
 export class CodeEditor {
   editor: Editor;
-  $: any;
   opts = {
     openState: { cv: '65%', pn: '35%' }, //State when open
     closedState: { cv: '85%', pn: '15%' }, //State when closed
   };
-  canvas: any;
-  panelViews: any;
-  isShowing: boolean;
+  canvas: HTMLElement;
+  panelViews: HTMLElement;
+  isActive = false;
 
   component: Component | undefined;
-  codePanel: any;
+  codePanel: HTMLElement;
   htmlCodeEditor: any;
   cssCodeEditor: any;
   previousHtmlCode: string;
   previousCssCode: string;
 
-  constructor(editor: Editor, opts) {
+  constructor(editor: Editor, opts: any) {
     this.editor = editor;
-    this.$ = editor.$;
     this.opts = {...this.opts, ...opts};
-    this.canvas = this.findWithinEditor(`.gjs-cv-canvas`);
-    console.log('canvas', this.canvas);
-    this.panelViews = this.findWithinEditor(`.gjs-pn-views-container`);
-    console.log('panelViews', this.panelViews);
-    this.isShowing = true;
+    this.canvas = this.editor.getEl()?.querySelector(`.gjs-cv-canvas`) as HTMLElement;
+    this.panelViews = this.editor.getEl()?.querySelector(`.gjs-pn-views-container`) as HTMLElement;
   }
 
-  findPanel() {
-    const pn = this.editor.Panels;
-    return pn.getPanel('views-container') || pn.addPanel({ id: 'views-container' });
-  }
-
-  findWithinEditor(selector) {
-    return this.$(selector, this.editor.getEl());
-  }
-
-  buildCodeEditor(type) {
-    const { editor } = this;
-
-    return editor.CodeManager.createViewer({
+  buildCodeEditor(type: string) {
+    return this.editor.CodeManager.createViewer({
       codeName: type === 'html' ? 'htmlmixed' : 'css',
-      theme: 'hopscotch',
+      // theme: 'hopscotch',
       readOnly: 0,
-      autoBeautify: 1,
-      autoCloseTags: 1,
-      autoCloseBrackets: 1,
-      styleActiveLine: 1,
-      smartIndent: 1
+      // autoBeautify: 1,
+      // autoCloseTags: 1,
+      // autoCloseBrackets: 1,
+      // styleActiveLine: 1,
+      // smartIndent: 1
     });
   }
 
-  buildSection(type, codeViewer) {
-    const { $ } = this;
-    const section = $('<section></section>');
-    section.append($(`
-      <div class="codepanel-separator">
-        <div class="codepanel-label">${type}</div>
-        <button class="cp-apply-${type} gjs-btn-prim">Apply</button>
-        </div>
-      </div>`));
-    const codeViewerEl = codeViewer.getElement();
-    codeViewerEl.style.height = 'calc(100% - 30px)';
-    section.append(codeViewerEl);
-    this.codePanel.append(section);
-    return section.get(0);
-  }
-
+  // add html/css code editor views-container, along with style, traits, components
   buildCodePanel() {
-    const { $, editor } = this;
-    const panel = this.findPanel();
-    this.codePanel = $('<div></div>');
-    this.codePanel.addClass('code-panel');
+    const buildSection = (type: string, codeViewer) => {
+      const section = document.createElement('section');
+      section.insertAdjacentHTML('beforeend', `
+        <div class="codepanel-separator">
+          <div class="codepanel-label">${type}</div>
+          <button class="apply-btn gjs-btn-prim">Apply</button>
+        </div>`);
+      const codeViewerEl = codeViewer.getElement();
+      codeViewerEl.style.height = 'calc(100% - 30px)';
+      section.appendChild(codeViewerEl);
+
+      const applyHandler = 
+        type === 'html' ? this.updateHtml :
+          type === 'css' ? this.updateCss : () => alert('apply handler error');
+      const applyBtn = section.querySelector('.apply-btn') as HTMLButtonElement;
+      applyBtn.addEventListener('click', applyHandler.bind(this));
+
+      return section;
+    }
 
     this.htmlCodeEditor = this.buildCodeEditor('html');
     this.cssCodeEditor = this.buildCodeEditor('css');
+    const htmlCodeSection = buildSection('html', this.htmlCodeEditor);
+    const cssCodeSection = buildSection('css', this.cssCodeEditor);
 
-    const sections = [this.buildSection('html', this.htmlCodeEditor), this.buildSection('css', this.cssCodeEditor)];
+    this.codePanel = document.createElement('div');
+    this.codePanel.classList.add('code-panel');
+    this.codePanel.append(htmlCodeSection, cssCodeSection);
 
-    panel && panel.set('appendContent', this.codePanel).trigger('change:appendContent');
+    const viewsContainer = this.editor.Panels.getPanel('views-container') as Panel;
+    viewsContainer
+      .set('appendContent', this.codePanel)
+      .trigger('change:appendContent');
     this.updateEditorContents();
 
-    this.codePanel.find('.cp-apply-html')
-      .on('click', this.updateHtml.bind(this));
-
-    this.codePanel.find('.cp-apply-css')
-      .on('click', this.updateCss.bind(this));
-
-    Split(sections, {
+    split([htmlCodeSection, cssCodeSection], {
       direction: 'vertical',
       sizes: [50, 50],
       minSize: 100,
@@ -102,31 +83,33 @@ export class CodeEditor {
       onDragEnd: this.refreshEditors.bind(this),
     });
 
-    editor.on('component:update', model => this.updateEditorContents());
-    editor.on('stop:preview', () => {
-      if (this.isShowing) {
-        this.canvas.css('width', this.opts.openState.cv);
+    this.editor.on('component:update', model => this.updateEditorContents());
+    this.editor.on('stop:preview', () => {
+      // const viewsContainerPanel = this.editor.Panels.getPanel('views-container');
+      // if (viewsContainerPanel?.view.className === '')
+      // console.log('>>>>>>..', this.editor.Panels.getPanel('views-container'));
+      if (this.isActive) {
+        this.canvas.style.width = this.opts.openState.cv;
       }
     });
   }
 
   showCodePanel() {
-    this.isShowing = true;
     this.updateEditorContents();
-    this.codePanel.css('display', 'block');
+    this.isActive = true;
     // make sure editor is aware of width change after the 300ms effect ends
     setTimeout(this.refreshEditors.bind(this), 320);
 
-    this.panelViews.css('width', this.opts.openState.pn);
-    this.canvas.css('width', this.opts.openState.cv);
+    this.panelViews.style.width = this.opts.openState.pn;
+    this.canvas.style.width = this.opts.openState.cv;
   }
 
   hideCodePanel() {
-    if (this.codePanel) this.codePanel.css('display', 'none');
-    this.isShowing = false;
+    this.codePanel.remove();
+    this.isActive = false;
 
-    this.panelViews.css('width', this.opts.closedState.pn);
-    this.canvas.css('width', this.opts.closedState.cv);
+    this.panelViews.style.width = this.opts.closedState.pn;
+    this.canvas.style.width = this.opts.closedState.cv;
   }
 
   refreshEditors() {
@@ -134,74 +117,33 @@ export class CodeEditor {
     this.cssCodeEditor.refresh();
   }
 
-  updateHtml(e) {
-    e?.preventDefault();
-    const { editor, component } = this;
+  updateHtml(event) {
+    event.preventDefault();
     let htmlCode = this.htmlCodeEditor.getContent().trim();
     if (!htmlCode || htmlCode === this.previousHtmlCode) return;
+
     this.previousHtmlCode = htmlCode;
-
-    let idStyles = '';
-    this.cssCodeEditor
-      .getContent()
-      .split('}\n')
-      .filter((el) => Boolean(el.trim()))
-      .map((cssObjectRule) => {
-        if (!(/}$/.test(cssObjectRule))) {
-          //* Have to check closing bracket existence for every rule cause it can be missed after split and add it if it doesnt match
-          return `${cssObjectRule}}`;
-        }
-      })
-      .forEach(rule => {
-        if (/^#/.test(rule))
-          idStyles += rule;
-      });
-
-    htmlCode += `<style>${idStyles}</style>`;
-
-    if (component?.attributes.type === 'wrapper') {
-      editor.setComponents(htmlCode);
-    } else {
-      editor.select(component?.replaceWith(htmlCode));
-    }
-    return htmlCode;
+    this.editor.select(this.component?.replaceWith(htmlCode));
   }
 
-  updateCss(e) {
-    e?.preventDefault();
+  updateCss(event) {
+    event.preventDefault();
     const cssCode = this.cssCodeEditor.getContent().trim();
     if (!cssCode || cssCode === this.previousCssCode) return;
+
     this.previousCssCode = cssCode;
     this.editor.Css.addRules(cssCode);
-    return cssCode;
-  }
-
-  getRules(rules, opts: any = {}) {
-    const { editor } = this;
-    const sm = editor.Selectors;
-    return rules.map((rule) => {
-      const selector = sm.get(rule.selectors);
-      const { state, selectorsAdd } = rule;
-      const { atRuleType, atRuleParams } = opts;
-      return (
-        selector &&
-        editor.Css.get(selector, state, atRuleParams, {
-          selectorsAdd,
-          atRule: atRuleType,
-        } as any)
-      );
-    });
   }
 
   updateEditorContents() {
-    if (!this.isShowing) return;
+    if (!this.editor.getSelected()) return;
 
-    this.component = this.editor.getSelected();
-    if (this.component) {
-      this.htmlCodeEditor.setContent(this.component.toHTML());
-      this.cssCodeEditor.setContent(this.editor.CodeManager.getCode(this.component, 'css', {
-        cssc: this.editor.Css
-      }));
-    }
+    this.component = this.editor.getSelected() as Component;
+    this.htmlCodeEditor.setContent(this.component.toHTML());
+
+    const css = this.editor.CodeManager.getCode(this.component, 'css', {
+      cssc: this.editor.Css
+    });
+    this.cssCodeEditor.setContent(css);
   }
 }
